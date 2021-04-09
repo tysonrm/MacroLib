1d46c3e06a0413814ecb45eae0d8f8fb367ce40e
tyson <tyson7757@comcast.net>
2021-04-09 01:40:27 -0500

    refresh from mod-fed

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
-const ContainerReferencePlugin = require("webpack").container
-  .ContainerReferencePlugin;
+const ModuleFederationPlugin = require("webpack").container
+  .ModuleFederationPlugin;
 const fetchRemotes = require("./webpack/fetch-remotes");
 const remoteEntries = require("./webpack/remote-entries");
+require("dotenv").config();
+
+const port = process.env.PORT || 8707;
+const sslPort = process.env.SSL_PORT || 8070;
+const sslEnabled = /true/i.test(process.env.SSL_ENABLED);
+const publicPort = sslEnabled ? sslPort : port;
 
 module.exports = () => {
   return new Promise(resolve => {
@@ -11,9 +17,9 @@ module.exports = () => {
         target: "async-node",
         mode: "development",
         devtool: "source-map",
-        entry: ["@babel/polyfill", path.resolve(__dirname, "src/index.js")],
+        entry: ["@babel/polyfill", path.resolve(__dirname, "src/server.js")],
         output: {
-          publicPath: "http://localhost:8070",
+          publicPath: `http://localhost:${publicPort}`,
           path: path.resolve(__dirname, "dist"),
           libraryTarget: "commonjs2",
         },
@@ -35,9 +41,20 @@ module.exports = () => {
           ],
         },
         plugins: [
-          new ContainerReferencePlugin({
+          new ModuleFederationPlugin({
+            name: "microlib",
+            filename: "remoteEntry.js",
+            library: {
+              name: "microlib",
+              type: "commonjs-module",
+            },
             remoteType: "commonjs-module",
-            remotes: remotes,
+            remotes,
+            exposes: {
+              "./server": "./src/server",
+              "./models": "./src/models",
+              "./remoteEntries": "./webpack/remote-entries",
+            },
           }),
         ],
       })
